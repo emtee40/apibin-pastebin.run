7879703cc0f96ee9d3cc4fddc6810ce2e1fc1f67
Kamila Borowska <kamila@borowska.pw>
2023-12-07 17:40:03 +0100

    Write test for uploading pastes

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -6,9 +6,12 @@ import { setupServer } from "msw/node";
 import { HttpResponse, http } from "msw";
 import { afterAll, afterEach, beforeAll } from "vitest";
 
-const server = setupServer(
+export const server = setupServer(
   http.get("https://pastebin.run/api/v1/languages", () =>
-    HttpResponse.json([{ identifier: "plaintext", name: "Plain text" }]),
+    HttpResponse.json([
+      { identifier: "plaintext", name: "Plain text" },
+      { identifier: "php", name: "PHP" },
+    ]),
   ),
   http.get("https://pastebin.run/hello.txt", () =>
     HttpResponse.text("Hello, world!"),
